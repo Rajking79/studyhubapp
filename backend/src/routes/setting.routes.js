315e8a7f246e35fb617a924fa0be28622efa4756
Rajking79 <bbr4693@gmail.com>
2026-08-04 04:57:52 -0400

    refactor: remove backend directory

--- backend/src/routes/setting.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getSettings, updateSettings } = require("../controllers/setting.controller");
-const { verifyJWT } = require("../middlewares/auth.middleware");
-
-router.get("/user/settings", verifyJWT, getSettings);
-router.patch("/user/settings", verifyJWT, updateSettings);
-
-module.exports = router;
