f60c58d1320e0066c90cdfeda6d31ab2cfaaa5e0
Rajking79 <bbr4693@gmail.com>
2026-08-04 06:04:17 -0400

    refactor: remove backend directory

--- backend/src/routes/googleAuth.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth.controller");
-
-router.post("/google-login", authController.googleLogin);
-
-module.exports = router;
